ec455fcef7db1d0702424e711bc6cc4a38ca99a0
it2-sr-ajmal <ajmalplyl@gmail.com>
2018-01-28 14:02:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
  <!DOCTYPE html>
+
+<!- sample angular project -->
 <html lang="en-US">
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
 <script src="angular.js"></script>
